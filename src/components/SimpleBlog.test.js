b7da95efb104d9d5b034d3c143496b761c51c2c8
Matti <matti.sillanpaa@sulake.com>
2018-12-09 01:14:45 +0200

    Add submission for assignment 5.13.

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -18,4 +18,22 @@ describe('<SimpleBlog />', () => {
     expect(titleAndAuthorDiv.text()).toContain(blog.author)
     expect(likesDiv.text()).toContain(blog.likes)
   })
+
+  it('calls like handler twice if like button is clicked twice', () => {
+    const blog = {
+      title: 'Komponenttitestaus tapahtuu jestillä ja enzymellä',
+      author: 'John Smith',
+      likes: 42
+    }
+
+    const mockHandler = jest.fn()
+    const blogComponent = shallow(<SimpleBlog blog={blog} onClick={mockHandler} />)
+
+    const button = blogComponent.find('button')
+    button.simulate('click')
+    button.simulate('click')
+    expect(mockHandler.mock.calls.length).toBe(2)
+
+  })
+
 })
