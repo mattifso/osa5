8af7dc8b51bd7bd96ba647143eff324140a4a5b9
Matti <matti.sillanpaa@sulake.com>
2018-12-09 13:47:49 +0200

    Add submission for assignment 5.16.

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -3,17 +3,40 @@ import { mount } from 'enzyme'
 import App from './App'
 import Blog from './Blog'
 import LoginForm from './LoginForm'
+import blogService from '../services/blogs'
 
 describe('<App />', () => {
   let app
-  beforeAll(() => {
-    localStorage.clear()
-    app = mount(<App />)
+  describe('when user is not logged', () => {
+    beforeEach(() => {
+      window.localStorage.removeItem('loggedBlogappUser')
+      app = mount(<App />)
+    })
+
+    it('renders login form and nothing else when user is not yet logged in', () => {
+      app.update()
+      expect(app.find(Blog).length).toEqual(0)
+      expect(app.find(LoginForm).length).toEqual(1)
+    })
   })
 
-  it('renders login form and nothing else when user is not yet logged in', () => {
-    app.update()
-    expect(app.find(Blog).length).toEqual(0)
-    expect(app.find(LoginForm).length).toEqual(1)
+  describe('when user is logged', () => {
+    beforeEach(() => {
+      const user = {
+        username: 'joe',
+        token: '345345345',
+        name: 'Joe Q. Public',
+        id: 'asdfq234534'
+      }
+
+      localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
+      app = mount(<App />)
+    })
+
+    it('renders the list of blogs when the user is logged in', () => {
+      app.update()
+      expect(app.find(Blog).length).toEqual(blogService.mockedBlogs.length)
+      expect(app.find(LoginForm).length).toEqual(0)
+    })
   })
 })
